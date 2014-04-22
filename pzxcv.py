fa1de5154fb45cdd950c2b706c7eec4a714f2fa0
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-04-22 17:59:10 +0900

    added auto characters adjustment feature.

--- pzxcv.py
+++ pzxcv.py
@@ -7,6 +7,7 @@ import ConfigParser
 import random
 
 
+MINIMUM_CHARACTERS_LENGTH = 5
 CONFIG_FILE = NICOBBS_CONFIG = os.path.dirname(os.path.abspath(__file__)) + '/pzxcv.config'
 
 
@@ -29,42 +30,93 @@ def main():
     config = ConfigParser.ConfigParser()
     config.read(CONFIG_FILE)
 
-    characters = config.get('pzxcv', 'characters')
+    default_characters = config.get('pzxcv', 'characters')
+    characters = default_characters
 
-    exit_count = 0      # never exit
+    total_input_count = 0
+
+    exit_count = 50     # character input count per 1 character set
     if len(sys.argv) == 2:
         exit_count = int(sys.argv[1])
 
-    input_count = 1
-    previours_character = None
-    reserved_character = None
+    should_exit = False
     getch = _Getch()
 
     while True:
-        target_character = None
-        if reserved_character:
-            target_character = reserved_character
-            reserved_character = None
-        else:
-            index = random.randint(0, len(characters) - 1)
-            target_character = characters[index]
-            if target_character == previours_character:
-                continue
-            previours_character = target_character
+        input_count = 0
+        previours_character = None
+        reserved_character = None
+        missed_characters = {}
 
-        sys.stdout.write(str(input_count) + ": " + target_character + " ")
-        input_character = getch()
-        sys.stdout.write(input_character + "\n")
+        while input_count < exit_count:
+            target_character = None
+            if reserved_character:
+                target_character = reserved_character
+                reserved_character = None
+            else:
+                index = random.randint(0, len(characters) - 1)
+                target_character = characters[index]
+                if target_character == previours_character:
+                    continue
+                previours_character = target_character
 
-        if input_character == 'q':
-            break
-        elif target_character != input_character:
-            sys.stdout.write("\a")
-            reserved_character = target_character
-        elif target_character == input_character:
-            if input_count == exit_count:
+            sys.stdout.write(str(total_input_count + 1) + ": " + target_character + " ")
+            input_character = getch()
+            sys.stdout.write(input_character + "\n")
+
+            if input_character == 'q':
+                should_exit = True
                 break
-            input_count += 1
+            elif target_character != input_character:
+                sys.stdout.write("\a")
+                if not target_character in missed_characters:
+                    missed_characters[target_character] = 0
+                missed_characters[target_character] += 1
+                reserved_character = target_character
+            elif target_character == input_character:
+                total_input_count += 1
+                input_count += 1
+
+        # report_missed_characters(missed_characters)
+
+        if should_exit:
+            break
+
+        characters = renew_characters(default_characters, missed_characters)
+        if characters != default_characters:
+            print("character set changed. < " + characters + " >")
+
+
+def report_missed_characters(missed_characters):
+    sorted_missed_characters = sorted(missed_characters.items(), key=lambda x:x[1], reverse=True)
+
+    if 0 == len(sorted_missed_characters):
+        print("no missed input.")
+    else:
+        print("missed {} input(s).".format(len(sorted_missed_characters)))
+        print("-----")
+        for character, missed_count in sorted_missed_characters:
+            print(character + ": " + str(missed_count))
+        print("-----")
+
+
+def renew_characters(default_characters, missed_characters):
+    failure_characters = ""
+    sorted_missed_characters = sorted(missed_characters.items(), key=lambda x:x[1])
+
+    for character, missed_count in sorted_missed_characters:
+        if 0 < missed_count:
+            failure_characters += character
+
+    characters = default_characters
+
+    if 0 < len(failure_characters):
+        characters = failure_characters
+        while len(characters) < MINIMUM_CHARACTERS_LENGTH:
+            index = random.randint(0, len(default_characters) - 1)
+            characters  += default_characters[index]
+
+    return characters
 
 
 if __name__ == "__main__":
